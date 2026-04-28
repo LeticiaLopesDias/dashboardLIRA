6ccedb53141bf88b23321d8432842eb45dac900c
Leticia <leticialssdias@outlook.com>
2026-04-28 16:31:41 +0200

    Initial commit

---
+++ deploy-dashboard.R
@@ -0,0 +1,10 @@
+# Conectar ao Git
+usethis::use_git()
+
+# Conectar ao GitHub
+usethis::use_github()
+
+# Repositório no GitHub criado!
+
+# Função para criar um arquivo README.md - descrever conteúdo do repositório
+usethis::use_readme_md()
